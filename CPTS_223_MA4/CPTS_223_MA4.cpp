2ef19d8494de02a766e7f4833324071b5da60408
eniol <eniol@HARRYSTYLESPC>
2023-11-20 21:42:15 -0800

    Add project files.

---
+++ CPTS_223_MA4/CPTS_223_MA4.cpp
@@ -0,0 +1,12 @@
+﻿// CPTS_223_MA4.cpp : Defines the entry point for the application.
+//
+
+#include "CPTS_223_MA4.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
